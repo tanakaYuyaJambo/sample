d5aa4f21ab06abadde4e7f3c84e264b274aca3ac
田中雄也 <>
2022-10-05 11:14:34 +0900

    自分で作った段階

---
+++ sample/CollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewCell.swift
+//  sample
+//
+//  Created by 田中雄也 on 2022/10/03.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
